b782bb656a71e68e2de855e000eb7d07e695a9ae
sebastienplottu <sebastien.plottu@ynov.com>
2020-05-15 16:24:02 +0200

    Suppression heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,9 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (env('REDIRECT_HTTPS')){
-            $this->app['request']->server->set('HTTPS', true);
-        }
+    
+        
     }
 
     /**
@@ -24,11 +22,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
-       if (env('REDIRECT_HTTPS')){
-           $url->formatScheme('https');
-       }
+      
     }
 
 
