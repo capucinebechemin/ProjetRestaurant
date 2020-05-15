7384e1959089e7025ff97c4b62f4d322b88593b4
sebastienplottu <sebastien.plottu@ynov.com>
2020-05-15 15:02:41 +0200

    modif hasmany

--- app/Client.php
+++ app/Client.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $fillable = [
-        'prenom', 'nom', 'adresse', 'solde'
+        'prenom', 'nom', 'adresse', 'solde','id_user'
     ];
+
+
+    public function client_user()
+    {
+        # la relation inverse se déclare grace a la méthode "hasMany", qui ne prend cette fois en paramètre, que le nom du model "A"
+        return $this->belongsTo(users::class, "id_user");
+    }
+
+
+
 }
--- app/User.php
+++ app/User.php
@@ -36,4 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function client_id()
+    {
+        # BelongsTo doit prendre en premier paramètre le nom du model A, puis en second paramètre, le nom du champs dans le modèle courant lié avec le model A grâce à sa foreign key
+        return $this->hasMany(client::class);
+    }
 }
