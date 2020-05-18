6e97d912a08375a6aad4b4b834fd6986c27032df
capucinebechemin <57672229+capucinebechemin@users.noreply.github.com>
2020-05-18 16:02:33 +0200

    turn form to french

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,12 +68,12 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Accueil</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">Se connecter</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">S'inscire</a>
                         @endif
                     @endauth
                 </div>
@@ -81,18 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    CoupFaim
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <p>Coup Faim fait le lien entre les restaurants et vous pour le bonheur de vos papilles !</p>
                 </div>
             </div>
         </div>
