a3f7aecd3fae98b00b7306ed812a01aab18d650c
sebastienplottu <sebastien.plottu@ynov.com>
2020-06-11 16:12:45 +0200

    Profile client fonctionnel

--- app/Http/Controllers/controllerClient.php
+++ app/Http/Controllers/controllerClient.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Client;
+use http\Client\Curl\User;
 use Illuminate\Http\Request;
 
 class controllerClient extends Controller
@@ -9,4 +11,40 @@ class controllerClient extends Controller
     public function indexResto(){
 
     }
+
+
+
+    public function profile(){
+        $user = \Auth::user();
+        $client = Client::where('id_user', $user->id)->first(); 
+        return view('client.profile',compact('client','user'));
+    }
+
+
+    
+public function update(Request $request){
+
+    $request->validate([
+        'prenom' => 'required',
+        'nom' => 'required',
+        'adresse' => 'required',
+        'solde' => 'required',
+    ]);
+
+    $user = \Auth::user();
+    $client = Client::where('id_user', $user->id)->first();
+
+    $client->prenom = $request->get('prenom');
+    $client->nom = $request->get('nom');
+    $client->adresse = $request->get('adresse');
+    $client->solde = $request->get('solde');
+    $client->id_user = $user->id;
+
+    $client->save();
+
+    return redirect()->route('home');
+
+}
+
+
 }
